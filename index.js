433de74f390d4825ddb3376285ba42a8e6d6361e
Esteban Garcia <egarciatx87@gmail.com>
2026-04-14 14:40:58 -0500

    Updated the code on index.js

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
+function createLoginTracker(userInfo) {
+  let attemptCount = 0;
 
+  return (passwordAttempt) => {
+    attemptCount++;
 
+    if (attemptCount > 3) {
+      return "Account locked due to too many failed login attempts";
+    }
+
+    if (passwordAttempt === userInfo.password) {
+      return "Login successful";
+    } else {
+      if (attemptCount === 3) {
+        return "Account locked due to too many failed login attempts";
+      }
+      return `Attempt ${attemptCount}: Login failed`;
+    }
+  };
+}
 
 module.exports = {
   ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
